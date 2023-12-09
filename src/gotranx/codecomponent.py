013bc04ddf7b95e33d51c6e65a8053ced908c8be
Henrik Finsberg <henrikfinsberg@hotmail.com>
2023-12-09 11:55:39 +0100

    Add missing codecomponent module

---
+++ src/gotranx/codecomponent.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+import typing
+import attr
+from .ode import ODE
+
+
+@attr.s(frozen=True, slots=True)
+class CodeComponent:
+    name: str = attr.ib()
+    ode: ODE = attr.ib()
+    function_name: str = attr.ib()
+    description: str = attr.ib()
+    params: typing.Optional[dict[str, typing.Any]] = attr.ib(default=None)
+
+
+def rhs_expressions(ode: ODE, function_name: str = "rhs", result_name: str = "dy", params=None):
+    descr = f"Compute the right hand side of the {ode} ODE"
+    return CodeComponent(
+        name="RHSComponent",
+        ode=ode,
+        function_name=function_name,
+        description=descr,
+        params=params,
+        **{result_name: ode.state_derivatives},
+    )
