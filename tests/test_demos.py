094930bbe3a023a243cd5bcb1b008fd6bed9b3a1
Henrik Finsberg <henrikfinsberg@hotmail.com>
2025-03-21 19:31:43 +0100

    Add marker for example tests

--- tests/test_demos.py
+++ tests/test_demos.py
@@ -16,6 +16,7 @@ def env():
     return env
 
 
+@pytest.mark.example
 @pytest.mark.skipif(sys.version_info < (3, 9), reason="requires python3.9 or higher")
 @pytest.mark.parametrize(
     "example",
